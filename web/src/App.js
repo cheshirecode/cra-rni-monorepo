99ce4bf8efb9090a51ed170691da4f054a796820
cheshireCode <dac4158@gmail.com>
2018-06-29 12:11:23 +0800

    remove react-native-rename

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import test from "core/test";
-alert(test);
 
 class App extends Component {
   render() {
@@ -14,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <p>core/test value - {test}</p>
         </p>
       </div>
     );
